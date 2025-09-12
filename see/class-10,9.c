e8b3764e455d50d37d90d11c296bc7c3682de76d
PalDPathak404 <pdpathak2005@gmail.com>
2025-09-12 14:39:22 +0530

    Myopia

---
+++ see/class-10,9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+    {
+        int arr1[] = {4,5,7,9,10};
+        int arr2[] = {0};
+        int j=1;
+        for(int i = 4;i >= 0; i--){
+            arr2[] = {arr1[i]};
+            printf("%d numbered element of second array is %d", j , arr2[]);
+        }
+    }
